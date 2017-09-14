42bd859cfdd996be912d8379c26050c3aad0486d
rekmor <RekMor@Rekmors-MacBook-Pro.local>
2017-09-13 19:41:35 -0700

    first commit

---
+++ src/main/java/com/example/newp/NewpApplication.java
@@ -0,0 +1,12 @@
+package com.example.newp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewpApplication.class, args);
+	}
+}
